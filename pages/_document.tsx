0d0683f3ff56a1fd98f5115d00888f86e9e4209d
jkchuynh829 <jkchuynh829@gmail.com>
2021-12-10 16:32:09 -0800

    Fix _document types

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import { extractCritical } from '@emotion/server'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
     const critical = extractCritical(initialProps.html)
     initialProps.html = critical.html
@@ -24,8 +24,8 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <style
-            data-emotion-css={this.props.ids?.join(' ')}
-            dangerouslySetInnerHTML={{ __html: this.props.css }}
+            data-emotion-css={(this.props as any).ids?.join(' ')}
+            dangerouslySetInnerHTML={{ __html: (this.props as any).css }}
           />
         </Head>
         <body>
