b5e70ec6476f678807452f4fc53f986a5f3c98b7
Jimmy Huynh <jkchuynh829@gmail.com>
2022-05-10 01:28:32 -0700

    Things to do

---
+++ pages/things-to-do.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import { useDispatch } from 'react-redux';
+import tw from 'twin.macro';
+import Maintenance from '../src/components/Maintenance';
+import ThingsToDo from '../src/components/ThingsToDo';
+import { useSelector } from '../src/store';
+import { disableMaintenanceMode } from '../src/store/maintenanceMode';
+
+const Container = tw.div`relative w-full h-full`;
+
+const Main = () => {
+  const dispatch = useDispatch();
+  const { maintenanceEnabled } = useSelector((state) => state.maintenanceMode);
+  return (
+    <Container>
+      <Head>
+        <title>Dorothy and Christopher&apos;s Wedding - Things to Do</title>
+        <meta name="description" content="Made by Jimmy Huynh" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {maintenanceEnabled ? (
+        <Maintenance unlock={() => dispatch(disableMaintenanceMode())} />
+      ) : (
+        <ThingsToDo />
+      )}
+    </Container>
+  );
+};
+
+export default Main;
