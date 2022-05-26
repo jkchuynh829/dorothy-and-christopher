021210b90046a2edc623e78b44d11007ba623a02
Jimmy Huynh <jkchuynh829@gmail.com>
2022-05-26 16:42:27 -0700

    Fix nav bar scroll for every page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,17 @@
 import { useRouter } from 'next/router';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 import { useSpring, animated } from 'react-spring';
 import tw, { styled } from 'twin.macro';
 import { useThrottledCallback } from 'use-debounce';
 import { useSelector } from '../store';
 import NavLink from './NavLink';
+
+const SCROLLED_PAGES = [
+  '/wedding-party',
+  '/faqs',
+  '/things-to-do',
+  '/registry',
+];
 
 const Container = animated(
   styled.nav`
@@ -26,13 +33,14 @@ const Navigation = () => {
 
   const { asPath } = useRouter();
 
-  const [scrolled, setScrolled] = useState(true);
+  const [scrolled, setScrolled] = useState(false);
 
   const onScroll = useThrottledCallback(() => {
+    const shouldScroll = SCROLLED_PAGES.includes(asPath);
     if (window.scrollY > 100 && !scrolled) {
       setScrolled(true);
     }
-    if (window.scrollY <= 100 && scrolled) {
+    if (window.scrollY <= 100 && scrolled && !shouldScroll) {
       setScrolled(false);
     }
   }, 100);
@@ -44,11 +52,15 @@ const Navigation = () => {
     };
   }, [onScroll]);
 
+  const shouldScroll = useMemo(() => {
+    return scrolled || SCROLLED_PAGES.includes(asPath);
+  }, [scrolled, asPath]);
+
   const navAnimation = useSpring({
-    height: scrolled ? '6rem' : '10rem',
-    color: scrolled ? '#0F0F0F' : '#FAFAFA',
-    background: scrolled ? '#FAFAFA' : 'transparent',
-    boxShadow: scrolled
+    height: shouldScroll ? '6rem' : '10rem',
+    color: shouldScroll ? '#0F0F0F' : '#FAFAFA',
+    background: shouldScroll ? '#FAFAFA' : 'transparent',
+    boxShadow: shouldScroll
       ? '0px 1px 3px rgba(15, 15, 15, 0.15)'
       : '0px 1px 3px rgba(0, 0, 0, 0)',
   });
@@ -64,7 +76,7 @@ const Navigation = () => {
 
   return (
     <Container style={navAnimation}>
-      {!scrolled && <Gradient />}
+      {!scrolled && !SCROLLED_PAGES.includes(asPath) && <Gradient />}
       <NavLink
         href="/"
         selected={isCurrentPath('/')}
@@ -78,7 +90,7 @@ const Navigation = () => {
       <NavLink href="/our-story" selected={isCurrentPath('/our-story')}>
         Our Story
       </NavLink>
-      <NavLink href="/things-to-do" selected={isCurrentPath('/things-to-do')}>
+      <NavLink href="/wedding-party" selected={isCurrentPath('/wedding-party')}>
         Wedding Party
       </NavLink>
       <NavLink href="/faqs" selected={isCurrentPath('/faqs')}>
@@ -89,9 +101,6 @@ const Navigation = () => {
       </NavLink>
       <NavLink href="/registry" selected={isCurrentPath('/registry')}>
         Registry
-      </NavLink>
-      <NavLink href="/wedding-party" selected={isCurrentPath('/wedding-party')}>
-        Wedding Party
       </NavLink>
     </Container>
   );
