94cb0a1ff250dd67646366af099068a8af2abfa4
Jimmy Huynh <jkchuynh829@gmail.com>
2022-05-31 23:40:12 -0700

    Update registry

--- src/components/GoldTextClip.tsx
+++ src/components/GoldTextClip.tsx
@@ -1,7 +1,8 @@
 import tw, { styled } from 'twin.macro';
 
-const Container = styled.div<{ clipText: boolean }>`
+const Container = styled.div<{ clipText: boolean; fullWidth: boolean }>`
   ${tw`px-5 py-10`} // prevent clipping
+  ${({ fullWidth }) => fullWidth && tw`px-0`};
   background: linear-gradient(
     25deg,
     #c39331,
@@ -16,12 +17,21 @@ const Container = styled.div<{ clipText: boolean }>`
 
 interface GoldTextClipBase {
   clipText?: boolean;
+  fullWidth?: boolean;
 }
 
 type GoldTextClipProps = React.PropsWithChildren<GoldTextClipBase>;
 
-const GoldTextClip = ({ children, clipText = true }: GoldTextClipProps) => {
-  return <Container clipText={clipText}>{children}</Container>;
+const GoldTextClip = ({
+  children,
+  clipText = true,
+  fullWidth = false,
+}: GoldTextClipProps) => {
+  return (
+    <Container clipText={clipText} fullWidth={fullWidth}>
+      {children}
+    </Container>
+  );
 };
 
 export default GoldTextClip;
--- src/components/Registry.tsx
+++ src/components/Registry.tsx
@@ -1,15 +1,25 @@
-import tw from 'twin.macro';
-import GoldTextClip from './GoldTextClip';
-import { H1, Paragraph, Aerotis, Almara } from './Typography';
+import tw, { styled } from 'twin.macro';
+import { H1 as Heading, Paragraph, Aerotis, Almara } from './Typography';
 import chrisPaypalQrCode from '../assets/registry/chris-paypal-qr-code.png';
 import chrisVenmoQrCode from '../assets/registry/chris-venmo-qr-code.png';
 import dorohyVenmoQrCode from '../assets/registry/dorothy-venmo-qr-code.png';
 import Image from 'next/image';
+import GoldTextClip from './GoldTextClip';
 
-const Container = tw.div`flex justify-center`;
-const Inner = tw.div`mt-52 text-center max-w-2xl px-6`;
-const Section = tw.div`mb-12`;
+const Container = tw.div`flex justify-center pb-16`;
+const Inner = tw.div`mt-36 text-center max-w-2xl px-6`;
+const Section = styled.div<{ isRow?: boolean }>`
+  ${tw`flex gap-x-12 gap-y-6 mb-4 justify-center items-center`}
+  ${({ isRow = false }) => (isRow ? tw`sm:flex-col flex-row` : tw`flex-col`)}
+`;
 const Seperator = tw.div`w-full h-0.5`;
+const H1 = tw(Heading)`mb-2`;
+
+const GoldenRule = () => (
+  <GoldTextClip clipText={false} fullWidth>
+    <Seperator />
+  </GoldTextClip>
+);
 
 const Registry = () => {
   return (
@@ -22,34 +32,30 @@ const Registry = () => {
           </Paragraph>
         </Section>
 
-        <GoldTextClip clipText={false}>
-          <Seperator />
-        </GoldTextClip>
+        <GoldenRule />
 
         <Section>
           <H1>
-            <Aerotis>
-              Venmo
-            </Aerotis>
+            <Aerotis>Venmo</Aerotis>
           </H1>
-          <Paragraph>
-            <Image src={chrisVenmoQrCode} />
-          </Paragraph>
-          <Paragraph>
-            <Image src={dorohyVenmoQrCode} />
-          </Paragraph>
+          <Section isRow>
+            <Image src={chrisVenmoQrCode} height="425px" width="325px" />
+            <Image src={dorohyVenmoQrCode} height="425px" width="325px" />
+          </Section>
         </Section>
+
+        <GoldenRule />
 
         <Section>
           <H1>
-            <Aerotis>
-              Paypal
-            </Aerotis>
+            <Aerotis>Paypal</Aerotis>
           </H1>
-          <Paragraph>
-            <Image src={chrisPaypalQrCode} />
-          </Paragraph>
+          <Section isRow>
+            <Image src={chrisPaypalQrCode} width="375px" height="375px" />
+          </Section>
         </Section>
+
+        <GoldenRule />
 
         <Section>
           <H1>
@@ -66,7 +72,8 @@ const Registry = () => {
             <Aerotis>Check</Aerotis>
           </H1>
           <Paragraph>
-            You can write us a check and make it out to "Christopher Cano" or "Dorothy Le".
+            You can write us a check and make it out to &quot;Christopher
+            Cano&quot; or &quot;Dorothy Le&quot;.
           </Paragraph>
         </Section>
       </Inner>
